686aa74a570bc45d0e5027ee7cebcaa121ddc6ca
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-05-14 00:14:11 +0200

    add navbar component & style

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <div className='navbar'>
+      <ul className='main-nav'>
+        <li><a className='title' href="/">SW</a></li>
+        <li><a href="/">Cards</a></li>
+        <li><a href="/">Series</a></li>
+        <li><a href="/">Interactive</a></li>
+        <li><a href="/">Company</a></li>
+      </ul>
+      <h1>StartWars Card Trade Widget </h1>
+    </div>
+  )
+}
+export default Header
